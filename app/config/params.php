2739883990333eddb7252ca0ce85e6d87e8f73bb
a.latukhin <a.latukhin@oos.ru>
2024-04-06 13:23:37 +0300

    - setting mailing

--- app/config/params.php
+++ app/config/params.php
@@ -3,7 +3,6 @@
 return [
     //'bsVersion' => '4.x', // this will set globally `bsVersion` to Bootstrap 4.x for all Krajee Extensions
     'bsDependencyEnabled' => false,
-    'adminEmail' => getenv('ADMIN_EMAIL'),
     'supportEmail' => getenv('SUPPORT_EMAIL'), // автоматическая отправка почты с данного емайл
     'secretKeyExpire' => 60 * 60,                    // время хранения секретного ключа
     'emailActivation' => true,                       // подтверждение нового пользователя по email
